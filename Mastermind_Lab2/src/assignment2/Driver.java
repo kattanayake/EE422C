b526769c4ae3483775cce925830d7e9909fcccd0
Kausthub Poondi <kpoondi@wireless-10-145-32-152.public.utexas.edu>
2017-02-12 19:02:09 -0600

    working version of Lab2

--- Mastermind_Lab2/src/assignment2/Driver.java
+++ Mastermind_Lab2/src/assignment2/Driver.java
@@ -2,7 +2,14 @@ package assignment2;
 
 public class Driver {
 	public static void main(String args[]) {
-		Game g = new Game(true);
+		boolean testing; 
+		if(args.length == 0 || !(args[0].equals('1'))) {
+			testing = false;
+		}
+		else {
+			testing = true;
+		}
+		Game g = new Game(testing);
 		System.out.println("Welcome to Mastermind.");
 		g.runGame();
 	}
