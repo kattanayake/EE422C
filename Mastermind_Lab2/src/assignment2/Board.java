94c0ea1055fafe21c8b9e804f28c6b0d34ac8484
Kausthub Poondi <kpoondi@wireless-10-145-32-152.public.utexas.edu>
2017-02-14 12:14:17 -0600

    commented

--- Mastermind_Lab2/src/assignment2/Board.java
+++ Mastermind_Lab2/src/assignment2/Board.java
@@ -5,18 +5,31 @@ import java.util.*;
 public class Board {
 	ArrayList<String> list;
 	
+	/**
+	 * Constructor for the board that creates an array list
+	 * to store all the game history
+	 */
 	public Board() {
 		list = new ArrayList<String>();
 	}
 	
+	/**
+	 * Adds a String to the ArrayList that contains the most 
+	 * recent guess and the resulting output
+	 * @param add A String that contains the most recent guess and clue
+	 */
 	public void addToHistory(String add) {
 		list.add(add);
 	}
 	
+	/**
+	 * Called when HISTORY is entered. Prints the entire game board
+	 * until that point in the game
+	 */
 	public void printHistory() {
 		for(int i = 0; i < list.size(); i++) {
 			System.out.println(list.get(i));
-		}
+		}		
 		System.out.println();
 	}	
 }
--- Mastermind_Lab2/src/assignment2/GameConfiguration.java
+++ Mastermind_Lab2/src/assignment2/GameConfiguration.java
@@ -2,11 +2,8 @@ package assignment2;
 /*  * EE422C Project 2 (Mastermind) 
  * Summer 2016
  */
-public class GameConfiguration {
-	public static final int guessNumber = 4; 
-	public static final String[] colors = {"Q","W","E","R","T","Y"}; 
-	public static final int pegNumber = 6;
-//	public static final int guessNumber = 12;
-//	public static final String[] colors = {"B","G","O","P","R","Y"};
-//	public static final int pegNumber = 4;
+public class GameConfiguration {	
+	public static final int guessNumber = 12;
+	public static final String[] colors = {"B","G","O","P","R","Y"};
+	public static final int pegNumber = 4;
 }
